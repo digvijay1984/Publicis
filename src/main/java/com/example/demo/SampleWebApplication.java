3b56d3d670a11f128eec52544d0ae28748b1f57e
digvijay1984 <99624146+digvijay1984@users.noreply.github.com>
2022-10-03 21:12:38 +0000

    Create SampleWebApplication.java

---
+++ src/main/java/com/example/demo/SampleWebApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebApplication.class, args);
+	}
+
+}
+Footer
+© 2022 GitHub, Inc.
